387ee9ca0baf200ff21f8543d78c312c262e7ebc
sudarshan <vemarapu.sudarshan@outlook.com>
2021-07-17 19:59:11 +0530

    design complete

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -3,7 +3,11 @@ let close = document.querySelectorAll('.hamburger img')[1];
 let main = document.querySelector('main');
 let footer = document.querySelector('footer');
 let navplate = document.querySelector('.navplate');
+let hero = document.querySelector('.hero');
+let space = document.querySelector('#space');
 
+space.classList.add('shadowspace');
+hero.classList.add('shadowhero');
 ham.classList.add('ham');
 close.classList.add('close');
 navplate.classList.add('navdisplay');
@@ -14,17 +18,22 @@ let shadowfoot = () => footer.classList.toggle('shadowfoot');
 let displaynav = () => navplate.classList.toggle('navdisplay');
 let displayclose = () => close.classList.toggle('close');
 let displayham = () => ham.classList.toggle('ham');
-
+let shadowhero = () => hero.classList.toggle('shadowhero');
+let shadowspace = () => space.classList.toggle('shadowspace');
 displaynav();
 displayclose();
 shadowmain();
 shadowfoot();
+shadowhero();
+shadowspace();
 ham.addEventListener('click',function(){
     displayham();
     displayclose();
     displaynav();
     shadowmain();
     shadowfoot();
+    shadowhero();
+    shadowspace();
 })
 
 close.addEventListener('click',function(){
@@ -33,4 +42,6 @@ close.addEventListener('click',function(){
     displaynav();
     shadowmain();
     shadowfoot();
+    shadowhero();
+    shadowspace();
 })
